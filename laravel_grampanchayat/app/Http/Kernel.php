b2b6085c386ba728e5958c1c924e2ffde2f7b65d
AgroZone Technology Pvt Ltd <officialagrozone@gmail.com>
2026-02-27 13:51:33 +0000

    dev: Full project setup - Backend FastAPI + MongoDB, Frontend React, Laravel CMS with SQLite - all services running on ports 8000/3000/8080

---
+++ laravel_grampanchayat/app/Http/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        // global middleware
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [],
+        'api' => [],
+    ];
+
+    protected $routeMiddleware = [
+        // route middleware
+    ];
+}
--- laravel_grampanchayat/bootstrap/app.php
+++ laravel_grampanchayat/bootstrap/app.php
@@ -1,22 +1,35 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin.auth' => \App\Http\Middleware\AdminAuth::class,
-            'super.admin' => \App\Http\Middleware\SuperAdminOnly::class,
-            'track.visitor' => \App\Http\Middleware\TrackVisitor::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The application instance is the central piece of the framework. Here we
+| will create the application instance and bind important interfaces into
+| the container so the framework can resolve them when needed.
+|
+*/
+
+$app = new Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+return $app;
